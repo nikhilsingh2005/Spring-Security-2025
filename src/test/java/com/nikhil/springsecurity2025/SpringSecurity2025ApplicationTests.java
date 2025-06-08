676ddd342dcf801c00bc348d9dc0cc5650e37d78
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-06-09 00:51:41 +0530

    Initial commit

---
+++ src/test/java/com/nikhil/springsecurity2025/SpringSecurity2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.springsecurity2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
